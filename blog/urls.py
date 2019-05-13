cb9479fc6458758f7e7b7ef0731507e6fa69ce98
Maple30 <st0963105799@gmail.com>
2019-05-13 21:44:08 +0800

    zzz

--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.nothing, name='nothing'),
+    path('index/', views.index, name='index'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,11 @@
 from django.shortcuts import redirect, render
 from django.utils import timezone
+from django.http import HttpResponse
 
 # Create your views here.
+
+
+def nothing(request):
+    return HttpResponse("你484想壞壞")
+def index(request):
+    return render(request, 'blog/index.html', {})
